c3e12465701b245a3993a8816ab8144a91828d13
Aboubakar2 <136275901+Aboubakar2@users.noreply.github.com>
2024-02-27 00:50:31 +0100

    Update settings.py

--- TenisClub/TenisClub/settings.py
+++ TenisClub/TenisClub/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-37-*e45%o-j$x3_(5dywywzxur0i%uvp+7)9irrxnqniyqe2cy'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
